716ff4d9086e2982d2572c1f3ced7fd02e832466
drfalev <107064841+drfalev@users.noreply.github.com>
2024-06-18 17:02:04 +0000

    list and table

--- tpapp.py
+++ tpapp.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+L = [
+  {
+    'a':'alma'
+    , 'b':'banán'
+    , 'c':'cseresznye'
+  }
+  , {
+    'a':'répa'
+    , 'b':'retek'
+    , 'c':'mogyoró'
+  }
+]
+
 @app.route("/")
 
 def hello_wolrld():
   # return "Hello World now"  
-  return render_template('home.html')
+  return render_template('home.html'
+                        , list=L
+                        )
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
